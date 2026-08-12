e467c72cc40bfebd348cabe09a37f825b49f70c8
DhanashreeNirmale <dhanashreenirmale@gmail.com>
2026-08-12 14:08:25 +0530

    Additionals

--- App/debug_resume.py
+++
@@ -1,8 +0,0 @@
-from utils import extract_text_from_file, clean_text, get_lines
-
-with open("your_resume_file.pdf", "rb") as f:
-    raw = extract_text_from_file(f)
-    raw = clean_text(raw)
-
-    for i, line in enumerate(get_lines(raw)):
-        print(i, repr(line))
